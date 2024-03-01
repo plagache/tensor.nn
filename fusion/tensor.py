e6fed80e89383eb7264d7eda43f9926d7dc1f9bf
plgch <plagache@protonmail.com>
2024-03-01 21:57:08 +0100

    how to backward __add__;

--- fusion/tensor.py
+++ fusion/tensor.py
@@ -1,55 +1,109 @@
-from numpy._typing import NDArray
-
-
 # class Context:
 #     def __init__(self, current, parents=(), operation=''):
 #         self.current = current
 #         self.parents = parents
 #         self.operation = operation
 
+# implement require gradient only in the case of training and backprog
+# not nescessary with only inference
+
 # will start with add and multiply
+# i need to decompose data in the class
+# we can moove the data as a memoryview()
+# and display the data with numpy
+# class should only stored what is nescessary to accomplish the backprog
+# the class describe how the backprog should work
+
+# data can be of multiple type / in 
+
+# type will decide what operation we have to do
+# shape
+# created
+#       operation that created it
+# create
+
+import numpy as np
+from numpy import dtype
+from typing import Tuple, Union
+
+default_type=np.float32
+
 class Tensor:
-    def __init__(self, ndarray: NDArray, _children=(), _operation=None):
-        self.ndarray = ndarray
-        self._previous = set(_children)
+    def __init__(self, data: Union[int, float, list, np.ndarray], _children=(), _operation=None):
+        self.need_gradient : bool = True
         # how can i zero_grad ?
         # i need to match shape for gradient i think
+        # self.gradient = Tensor(0)
         self.gradient = 0
+
+        # Context: internal variables used for autograd graph construction
+        # maybe create a new class for this
+        self._parents = set(_children)
         self._backward = lambda: None
         self._operation = _operation
-        self._shape = self.ndarray.shape
 
-    def __add__(self, other):
-        # here the forward, how do we calculate the forward
-        output = Tensor(self.ndarray + other.ndarray, (self, other), '+')
-        def _backward():
-            self.gradient += output.gradient
-            other.grad += output.gradient
-        return output
+
+        if isinstance(data, (int, float)):
+            self.ndata = np.array(data)
+            return
+
+        if isinstance(data, list):
+            self.ndata = np.array(data)
+            return
+
+        if isinstance(data, np.ndarray):
+            self.ndata = data
+            return
+
+    # @property is just a getter() | in our case it gets the shape()
+    @property
+    def shape(self) -> Tuple[int, ...]: return self.ndata.shape
+
+    @property
+    def dtype(self) -> dtype: return self.ndata.dtype
+    # def dtype(self) -> dtype: return np.dtype(self.ndata)
+
+    def numpy(self) -> np.ndarray: return self.ndata
 
     # define topological sort and then apply the _backward for each element in reversed
     # def backward(self):
+
+    def topological_sort(self):
+        def _topological_sort(node, node_visited, graph_sorted):
+            if node not in node_visited:
+                node_visited.add(node)
+                for parent in node._parents:
+                    _topological_sort(parent, node_visited, graph_sorted)
+                graph_sorted.append(node)
+            return graph_sorted
+        return _topological_sort(self, set(), [])
+
 
     def backward(self):
 
         # The first gradient is always 1.
         self.gradient = 1
+        # self.gradient = Tensor(1)
 
-        graph_sorted = []
-        node_visited = set()
-        def topological_sort(node):
-            if node not in node_visited:
-                node_visited.add(node)
-                for parent in node._previous:
-                    topological_sort(parent)
-                graph_sorted.append(node)
-
-        topological_sort(self)
+        # self.topological_sort()
+        # topological_sort(self)
         # print(*graph_sorted, sep = "\n")
 
-        for node in reversed(graph_sorted):
+        for node in reversed(self.topological_sort()):
             # print(node, "\n")
             node._backward()
+            # self.gradient = node._backward()
+
+    def __add__(self, other):
+        output = Tensor(self.ndata + other.ndata, (self, other), '+')
+        def _backward():
+            self.gradient += output.gradient
+            other.gradient += output.gradient
+        output._backward = _backward
+        return output
+        # return self.ndata + other.ndata
 
     def __repr__(self) -> str:
-        return f'[Tensor(shape={self._shape}, operation={self._operation}]'
+        # assert self.ndata.shape is not None
+        # return f'[Tensor(shape={self.ndata.shape}, operation={self._operation}]'
+        return f'<Tensor(ndata={self.ndata}, operation={self._operation}>'
--- simple_operation.py
+++ simple_operation.py
@@ -4,18 +4,40 @@ from fusion import Tensor
 
 # m = 4096
 m = 3
+t = np.random.rand(m, m).astype(np.float32)
 
-matrice_1 = Tensor(np.random.rand(m, m).astype(np.float32))
-matrice_2 = Tensor(np.random.rand(m, m).astype(np.float32))
+x = Tensor([[1,1,0],[1,0,1],[0,1,1]])
+y = Tensor([[2.0,0,-2.0]])
 
+matrice_1 = Tensor(t)
+matrice_2 = Tensor(t)
 matrice_3 = matrice_1 + matrice_2
-matrice_4 = matrice_3 + matrice_2
+# matrice_4 = matrice_3 + matrice_2
 
+z = y + x
+z.backward()
 
-print(matrice_1)
-print(matrice_2)
-print(matrice_3)
-print(matrice_4)
+print(z)
+print(z.dtype)
+print(z.shape)
+print(z.numpy())
+print(x.gradient)
+print(y.gradient)
+print(z.gradient)
+# print(x)
+# print(y)
+# print(y.dtype)
+# print(x.dtype)
+# print(y.shape)
+# print(x.shape)
+# print(x.ndata)
+# print(y.ndata)
+
+# print(matrice_1)
+# print(matrice_2)
+# print(matrice_3)
+# print(matrice_4)
+# print(matrice_4.gradient)
 
 # from tinygrad import Tensor
 #
