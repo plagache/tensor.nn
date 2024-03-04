463ce88d50a7ea135bc3518c3adb449ea90edc95
plgch <plagache@protonmail.com>
2024-03-04 22:28:52 +0100

    add Function class;

--- fusion/tensor.py
+++ fusion/tensor.py
@@ -18,24 +18,35 @@ default_type=np.float32
 # Forward create context attach to Tensors / parents / ops
 # Backward describe how the gradient is populated
 
+class Function:
+    def __init__(self, *tensors:Tensor):
+        self.parents = tensors
+
+        # Context: internal variables used for autograd graph construction
+        # maybe create a new class for this
+        # self._parents = set(_children)
+        # self._backward = lambda: None
+        # self._operation = _operation
+
+    # *args = positional arguments
+    # **kwargs = keyword arguments
+    def forward(self, *args, **kwargs): raise NotImplementedError("forward not implemented")
+    def backward(self, *args, **kwargs): raise NotImplementedError("backward not implemented")
+
+    # @classmethod provides a way to define methods that operate on the class itself rather than instances of the class.
+    @classmethod
+    def apply(function:Type[Function]):
+        # context is on the class level not the instance level
+        return
 
 class Tensor:
     def __init__(self, data: Union[int, float, list, np.ndarray], _children=(), _operation=None):
         # True for training / False for inference
         self.need_gradient : bool = True
 
-        # to zero_grad
+        # to zero_grad / maybe we don't need / we should just assume zero is always the case
         # we create a copy in shape of the tensor and zeroed all the value
-        self.gradient = Optional[Tensor]
-        # self.gradient = 0
-        # self.gradient = np.zeros_like(self.ndata)
-
-        # Context: internal variables used for autograd graph construction
-        # maybe create a new class for this
-        self._parents = set(_children)
-        self._backward = lambda: None
-        self._operation = _operation
-
+        self.gradient : Optional[Tensor] = None
 
         if isinstance(data, (int, float)):
             self.ndata = np.array(data)
@@ -58,11 +69,9 @@ class Tensor:
     def dtype(self) -> dtype: return self.ndata.dtype
     # def dtype(self) -> dtype: return np.dtype(self.ndata)
 
-    def numpy(self) -> np.ndarray: return self.ndata
+    # def numpy(self) -> np.ndarray: return self.ndata
     # def numpy(self) -> np.ndarray: return self.lazydata.numpy()
 
-    # define topological sort and then apply the _backward for each element in reversed
-    # def backward(self):
 
     def topological_sort(self):
         def _topological_sort(node, node_visited, graph_sorted):
@@ -75,42 +84,23 @@ class Tensor:
         return _topological_sort(self, set(), [])
 
 
+    # Create new Tensor at each node with the gradients
     def backward(self):
 
-        # The first gradient should always be 1.
-        # And a scalar value / Loss calculated : 0.80 something
-        # in a tensor and backpropagate to the biggest shape() of inputs X
-        # self.gradient = 1
-        # self.gradient = 1
-        # self.gradient = Tensor(1)
-        self.gradient = np.ones_like(self.ndata)
+        # First gradient is always one
+        self.gradient = Tensor(1)
 
-        # self.topological_sort()
-        # topological_sort(self)
-        # print(*graph_sorted, sep = "\n")
-
+        # for each node we want te create a Parents Tensor Gradients
         for node in reversed(self.topological_sort()):
-            # print(node, "\n")
-            node._backward()
-            # self.gradient = node._backward()
+            gradients = node._backward()
 
-    def sum(self):
-        output = Tensor(self.ndata.sum(), (self, ), 'sum')
-        def _backward():
-            self.gradient += output.gradient
-        output._backward = _backward
-        return output
+    # def sum(self):
 
-    def __add__(self, other):
-        output = Tensor(self.ndata + other.ndata, (self, other), '+')
-        def _backward():
-            self.gradient += output.gradient
-            other.gradient += output.gradient
-        output._backward = _backward
-        return output
-        # return self.ndata + other.ndata
+    # def __add__(self, other):
+ 
 
     def __repr__(self) -> str:
         # assert self.ndata.shape is not None
         # return f'[Tensor(shape={self.ndata.shape}, operation={self._operation}]'
-        return f'<Tensor(ndata={self.ndata}, operation={self._operation}>'
+        # we do not store operation on the Tensor: its in Function
+        return f'<Tensor(ndata={self.ndata}>'
