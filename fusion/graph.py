8c0a1ef564a6f4d995037f06fba5f5b853fd9e84
plgch <plagache@protonmail.com>
2024-09-17 11:13:08 +0200

    update indent;

--- fusion/graph.py
+++ fusion/graph.py
@@ -1,47 +1,49 @@
 from graphviz import Digraph
 
+
 def to_graphviz_label(self):
-    label = f"{self.shape}\n"
-    label += f"{self.dtype}\n"
-    if self._context:
-        label += f"{self._context.__class__.__name__}"
-    return f"<{label}>"
+  label = f"{self.shape}\n"
+  label += f"{self.dtype}\n"
+  if self._context:
+    label += f"{self._context.__class__.__name__}"
+  return f"<{label}>"
+
 
 def topological_sort(self):
-    def _topological_sort(node, node_visited, graph_sorted):
-        if node not in node_visited:
-            if getattr(node, "_context", None):
-                node_visited.add(node)
-                for parent in node._context.parents:
-                    _topological_sort(parent, node_visited, graph_sorted)
-                graph_sorted.append(node)
-        return graph_sorted
+  def _topological_sort(node, node_visited, graph_sorted):
+    if node not in node_visited:
+      if getattr(node, "_context", None):
+        node_visited.add(node)
+        for parent in node._context.parents:
+          _topological_sort(parent, node_visited, graph_sorted)
+        graph_sorted.append(node)
+    return graph_sorted
 
-    return _topological_sort(self, set(), [])
+  return _topological_sort(self, set(), [])
 
 
 def draw_graph(root, filename, format="svg", rankdir="LR"):
-    """
-    format: png | svg | ...
-    rankdir: TB (top to bottom graph) | LR (left to right)
-    """
-    assert rankdir in ["LR", "BT"]
-    nodes = topological_sort(root)
-    dot = Digraph(filename=filename, format=format, graph_attr={"rankdir": rankdir})  # , node_attr={'rankdir': 'TB'})
+  """
+  format: png | svg | ...
+  rankdir: TB (top to bottom graph) | LR (left to right)
+  """
+  assert rankdir in ["LR", "BT"]
+  nodes = topological_sort(root)
+  dot = Digraph(filename=filename, format=format, graph_attr={"rankdir": rankdir})  # , node_attr={'rankdir': 'TB'})
 
-    for node in reversed(nodes):
-        # print(to_graphviz_label(node))
-        # dot.node(node.__repr__())
-        dot.node(to_graphviz_label(node))
-        for parent in node._context.parents:
-            dot.edge(to_graphviz_label(parent), to_graphviz_label(node), label=node._context.__class__.__name__)
-            # dot.edge(parent.__repr__(), node.__repr__(), label=node._context.__class__.__name__)
+  for node in reversed(nodes):
+    # print(to_graphviz_label(node))
+    # dot.node(node.__repr__())
+    dot.node(to_graphviz_label(node))
+    for parent in node._context.parents:
+      dot.edge(to_graphviz_label(parent), to_graphviz_label(node), label=node._context.__class__.__name__)
+      # dot.edge(parent.__repr__(), node.__repr__(), label=node._context.__class__.__name__)
 
-    ## print the DOT source that will be put in the file
-    # print(dot.source)
+  ## print the DOT source that will be put in the file
+  # print(dot.source)
 
-    ## this will save and render the graph
-    dot.render(directory="/tmp", view=True)
-    # dot.render(directory="/tmp", view=False)
+  ## this will save and render the graph
+  dot.render(directory="/tmp", view=True)
+  # dot.render(directory="/tmp", view=False)
 
-    return dot
+  return dot
